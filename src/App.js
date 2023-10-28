55f2cf1183e794d2ee883678a0f2d9e44981086c
kabeerMansoor <kabeermansoor5@gmail.com>
2023-10-28 12:00:53 +0500

    DONE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputdata,setInputData] = useState('');
+  const [arrdata,setArrData] = useState('');
+
+  const addTudo=()=>{
+    setArrData ((prev)=>{
+      return [...prev,inputdata]
+    })
+    setInputData("");
+  }
+
+  const editData=(i)=>{
+    const a = prompt("Enter updated task:");
+    const arrNew = [...arrdata]
+    arrNew[i]=a;
+    setArrData(arrNew);
+  }
+
+
+  const deleteData=(index)=>{
+    const a = arrdata.filter((e,i)=>i!==index);
+    setArrData(a);
+  }
+
+  const deleteAll=()=>{
+    setArrData("");
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <input type='text' placeholder='Enter Tudo' onChange={(e)=>setInputData(e.target.value)} value={inputdata}  className='screen'/>
+      <div className='mainbtn'>
+      <button onClick={addTudo} className='btn green'>Add</button>
+      <button onClick={deleteAll} className='btn red'>DeleteAll</button>
+      </div>
+     
+      <ul>
+        {
+          arrdata.length? arrdata.map((e,i)=> <li key={i} className='margintop'>{e}  <button onClick={()=>editData(i)} className='btn blue'>Update</button>  <button onClick={()=>deleteData(i)} className='btn red'>Delete</button> </li>):"NO TODOS"
+        }
+      </ul>
     </div>
   );
 }
